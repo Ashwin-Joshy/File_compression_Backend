54159f14eeff13cca042e6e92dfe40391e2d62b7
Ashwin Joshy <54849409+Ashwin-Joshy@users.noreply.github.com>
2022-10-27 16:51:21 +0530

    Update some bugs

---
+++ controllers/fileController.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const { promises: fsp } = require('fs');
+const archiver = require('archiver');
+const readline = require('readline');
+const { resolve } = require('path');
+const fileController = {
+    archiveFile: async (fromPath, toPath, fromD, toD, zipName) => {
+        return new Promise(async (resolve, reject) => {
+            let output;
+            let toDate = new Date(toD)
+            console.log('toDate' + toDate);
+            let fromDate = new Date(fromD)
+            console.log('fromDate' + fromDate);
+            try {
+                const files = await fsp.readdir(fromPath)
+                if (files.length == 0) {
+                    reject('No files found');
+                }
+                //Step 2 - create a file to stream archive data to
+
+                output = fs.createWriteStream(`${toPath}/${zipName}.zip`);
+                const archive = archiver('zip', {
+                    zlib: { level: 9 }
+                });
+
+                //Step 3 - callbacks 
+                output.on('close', () => {
+                    console.log('Archive finished.');
+                    //output.end();
+                    resolve('Done')
+                });
+
+                archive.on('error', (err) => {
+                    console.log('throwing error');
+                    throw err;
+                });
+                //Step 4 - pipe and append files
+                archive.pipe(output);
+                for (let i = 0; i < files.length; i++) {
+                    //await fileController.validateFile(`${fromPath}/${files[i]}`);
+                    let fileDate;
+                    let stats=fs.statSync(`${fromPath}/${files[i]}`)
+                    fileDate = new Date(stats.mtimeMs);
+                    console.log(fileDate);
+                    console.log('Less than to date', fileDate < toDate);
+                    console.log('Greater than from date', fileDate > fromDate);
+                    if (fileDate > fromDate && fileDate <= toDate) {
+                        archive.append(fs.createReadStream(`${fromPath}/${files[i]}`), { name: `${files[i]}` });
+                    }
+
+                }
+
+                //Step 5 - finalize
+                archive.finalize();
+
+            }
+            catch (error) {
+                console.log('error', error);
+                //output.emit('close')
+                output.end()
+                await fsp.rm(`${toPath}/${zipName}.zip`)
+                reject(error)
+            }
+        })
+    },
+    validateFile: (filename) => {
+        //Function: To read a given file line by line and validate the file
+        //Input: filename to be validated
+        //Output: Promise which contain the result of validation
+        //Step1: Read the file line by line
+        //Step2: check for the number of HDR, TDR, Line Count
+        //Step3: If HDR, TDR count go above 1 or line count is more than 2 then file is not valid
+        //Step4: If file reached end satisfying the above condition then file is valid
+        return new Promise((resolve, reject) => {
+            const rl = readline.createInterface({
+                input: fs.createReadStream(filename),
+                crlfDelay: Infinity
+            });
+            let hdrCount, tdrCount, lineCount = 0;
+            rl.on('line', (line) => {
+                console.log(line);
+                lineCount++;//Check for no of line
+                if (hdrCount > 1 || tdrCount > 1 || lineCount > 2 || (!line.includes('HDR') && !line.includes('TDR'))) {
+                    console.log('File is not valid', line);
+                    reject('Validation Failed')
+                }
+                if (line.includes('HDR')) {
+                    hdrCount++;//To count the number of HDR
+                }
+                else if (line.includes('TDR')) {
+                    tdrCount++;//To count the number of TDR
+                }
+            });
+            rl.on('close', () => {//Check for end of file
+                console.log('Validation Success');
+                resolve('Validation Success')  //return
+            })
+        })
+    },
+}
+module.exports = fileController;
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const fileController = require('../controllers/fileController');
+
+/* GET home page. */
+router.post('/', async function (req, res, next) {
+  let data;
+  console.log(req.body);
+  console.log();
+  try {
+    let from = req.body.from;
+    let to = req.body.to;
+    let fromDate = req.body.fromdate;
+    let toDate = req.body.todate;
+    let zipName=req.body.zipname;
+    data = await fileController.archiveFile(from, to, fromDate, toDate, zipName);
+  }
+  catch (err) {
+    data = err
+  }
+  finally {
+    res.send(data)
+  }
+});
+
+module.exports = router;
